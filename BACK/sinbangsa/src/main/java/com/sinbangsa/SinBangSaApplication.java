35012a6093d4e05ea2184525f9d9fea7ee793049
dm10802 <dm10802@gmail.com>
2022-10-31 10:10:38 +0900

    [No issue][BE-찬기] chore : 폴더 구조 생성

---
+++ BACK/sinbangsa/src/main/java/com/sinbangsa/SinBangSaApplication.java
@@ -0,0 +1,12 @@
+package com.sinbangsa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinBangSaApplication {
+    public static void main(String[] args){
+        SpringApplication.run(SinBangSaApplication.class, args);
+    }
+
+}
