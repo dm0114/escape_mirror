6a4b89a6dfb43403858239e7c62c784bcac40baf
dm10802 <dm10802@gmail.com>
2022-10-31 11:33:05 +0900

    feat: User Entity

---
+++ BACK/sinbangsa/src/main/java/com/sinbangsa/data/entity/User.java
@@ -0,0 +1,53 @@
+package com.sinbangsa.data.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private  long id;
+
+    @Column
+    @NotNull
+    private String email;
+
+    @Column
+    @NotNull
+    private boolean admin;
+
+    @Column
+    @NotNull
+    private String tel;
+
+    @Column
+    @NotNull
+    private LocalDate birth;
+
+    @Column
+    @NotNull
+    private String password;
+
+    @Column
+    private String auth;
+
+    @Column
+    private String profile;
+
+    @Column
+    private Integer grade = 0;
+
+    @Column
+    private String Title;
+
+}
