a9b7d426fcbbd3f22c1a228e0784b1844ab84c07
dm10802 <dm10802@gmail.com>
2022-11-02 14:41:56 +0900

    [S07P31C104-186][FE-덕민] feat: 예약 페이지 연결

--- FRONT/escape_mobile/src/navigation/Root.js
+++ FRONT/escape_mobile/src/navigation/Root.js
@@ -5,11 +5,16 @@ import { createStackNavigator } from '@react-navigation/stack';
 import Stack from './Stack'
 import SearchScreen from '../screens/SearchScreen';
 import ThemeDetailScreen from '../screens/ThemeDetailScreen';
+import CafeDetailScreen from '../screens/CafeDetailScreen';
+
 import ReservationScreen from '../screens/ReservationScreen';
 import ReservationDetailScreen from '../screens/ReservationDetailScreen';
-import CafeDetailScreen from '../screens/CafeDetailScreen';
 import ReservationTransfer from '../screens/ReservationTransfer';
 import PostReservationScreen from '../screens/PostReservationScreen';
+
+import CommunityScreen from '../screens/CommunityScreen';
+import CommunityDetailScreen from '../screens/CommunityDetailScreen';
+import TabViewExample from './Tabview';
 
 
 const Nav = createStackNavigator()
@@ -18,7 +23,7 @@ const Nav = createStackNavigator()
 const Root = () => 
   // <Nav.Navigator screenOptions={{ presentation: "modal", headerShown: false }}>
   <Nav.Navigator
-    initialRouteName="ThemeDetailScreen"
+    initialRouteName="TabViewExample"
     screenOptions={{
       cardStyle: { backgroundColor: '#212121' },
       headerShown: false
@@ -26,13 +31,19 @@ const Root = () =>
   >
       {/* <Nav.Screen name="Drawer" component={Drawer}/> */}
       
+      <Nav.Screen name="TabViewExample" component={TabViewExample} />
       <Nav.Screen name="SearchScreen" component={SearchScreen} />
+      <Nav.Screen name="ThemeDetailScreen" component={ThemeDetailScreen} />
+      <Nav.Screen name="CafeDetailScreen" component={CafeDetailScreen} />
+
       <Nav.Screen name="ReservationScreen" component={ReservationScreen} />
       <Nav.Screen name="PostReservationScreen" component={PostReservationScreen} />
       <Nav.Screen name="ReservationDetailScreen" component={ReservationDetailScreen} />
       <Nav.Screen name="ReservationTransfer" component={ReservationTransfer} />
-      <Nav.Screen name="ThemeDetailScreen" component={ThemeDetailScreen} />
-      <Nav.Screen name="CafeDetailScreen" component={CafeDetailScreen} />
+
+      <Nav.Screen name="CommunityScreen" component={CommunityScreen} />
+      <Nav.Screen name="CommunityDetailScreen" component={CommunityDetailScreen} />
+
       <Nav.Screen name="Stack" component={Stack} />
   </Nav.Navigator>
 
