8216e24aec7e12d04d9becb56849a36ca3519b74
dm10802 <dm10802@gmail.com>
2022-11-06 23:43:44 +0900

    [S07P31C104-162][FE-윤주]feat: 도감 레이아웃 수정

--- FRONT/escape_mobile/src/screens/Book/RegionBook.jsx
+++ FRONT/escape_mobile/src/screens/Book/RegionBook.jsx
@@ -6,8 +6,8 @@ import theme from '../../../theme';
 import BottomSheet, { BottomSheetFlatList } from '@gorhom/bottom-sheet';
 import RNPickerSelect from 'react-native-picker-select';
 
-import {Checkbox} from 'native-base';
-import {Picker} from 'react-native-ui-lib';
+import { Modal } from "native-base";
+import {ProgressBar} from 'react-native-ui-lib';
 
 const RegionList = {
     // 서울
@@ -114,7 +114,7 @@ const data = [
     'themeId':2,
     'themeImg':'https://pbs.twimg.com/media/E80HdMrUcAQv4hi.jpg',
     'themeName':'새벽 베이커리',
-    'isClear':1
+    'isClear':2
     },
     {
     'themeId':3,
@@ -126,7 +126,7 @@ const data = [
     'themeId':4,
     'themeImg':'https://pbs.twimg.com/media/E80HdMrUcAQv4hi.jpg',
     'themeName':'새벽 베이커리',
-    'isClear':1
+    'isClear':2
     },
     {
     'themeId':5,
@@ -139,13 +139,13 @@ const data = [
 export default function RegionBook({navigation, route}){
     const {num, name} = route.params;
     const [selectRegion, setSelectRegion] = useState();
+    const [specificRegion, setSpecificRegion] = useState(null);
     const [cafeSet, setCafeSet] = useState(null);
+    const opacity_list = [0.8, 0.5, 0]
     const itemsList = RegionList[num].map((item) => {
         return {label:item, value:item}
     })
-    const bottomSheetRef = useRef(null);
-    // variables
-    const snapPoints = useMemo(() => ['3%', '85%'], []);
+    const [reviewModal, setReviewModal] = useState(false);
 
     useEffect(()=>{
         itemsList.length ? setSelectRegion(`${name}/null`) : setSelectRegion(`${name}`)
@@ -153,108 +153,122 @@ export default function RegionBook({navigation, route}){
 
     return(
         <ImageBackground source={{uri:testUnity}} style={{flex:1}}>
-            <View style={{justifyContent:'flex-start', flex:1, flexDirection:'column'}}>
-                <View>
+            <View style={{justifyContent:'flex-start', flex:1, flexDirection:'column', padding:20}}>
+                <Modal isOpen={reviewModal} onClose={()=>setReviewModal(false)}>
+                    <Modal.Content>
+                        <Modal.CloseButton />
+                        <Modal.Header>테마이름</Modal.Header>
+                        <Modal.Body>
+                            리뷰 내용이 들어가야 함
+                        </Modal.Body>
+                    </Modal.Content>
+                </Modal>
+                <View style={{flex:1}}>
                     <RoomNumber>ROOM {num}</RoomNumber>
                 </View>
-                <BottomSheet
-                    ref={bottomSheetRef}
-                    index={0}
-                    snapPoints={snapPoints}
-                    backgroundStyle={styles.bottomSheet}
+                <View style={{flex:8}}
                 >
                     {cafeSet !== null ? 
                     <>
-                        <CafeNavBtn style={{flex:1, flexDirection:'row', marginLeft:20, marginRight:20, marginTop:20}}
-                        onPress={()=>{navigation.navigate('CafeDetailScreen', {
+                        <View style={{marginTop:10}}>
+                        <CafeNavBtn style={{flexDirection:'row', marginTop:20, marginBottom:20}}
+                        onPress={()=>{
+                            // setCafeSet(null)
+                            navigation.navigate('CafeDetailScreen', {
                             storeId:cafeSet.storeId
-                        })}}>
+                        })
+                        }}>
                             <CafeImg source={{uri:cafeSet.storeImg}} resizeMode="cover" imageStyle={{borderRadius:10}} />
-                            <CafeName>{cafeSet.storeName}</CafeName>
+                            <View style={{flexDirection:'column', marginLeft:20, flex:1, marginTop:'auto', marginBottom:'auto'}}>
+                                <CafeName>{cafeSet.storeName}</CafeName>
+                                <View style={{flexDirection:'row', justifyContent:'space-between'}}>
+                                    <ProgressTitle>진행도</ProgressTitle>
+                                    <ProgressTitle style={{marginRight:10}}>10/15</ProgressTitle>
+                                </View>
+                                <ProgressBar progress={55} progressColor={'red'} style={{height:20}} />
+                            </View>
                         </CafeNavBtn>
-                        <View style={{flex:6, marginTop:10}}>
-                        <BottomSheetFlatList
-                            numColumns={2}
+                        <FlatList
+                            style={{marginTop:20}}
+                            // columnWrapperStyle={{ justifyContent:"space-between" }}
+                            keyExtractor={(item) => item.themeId}
                             data={data}
-                            contentContainerStyle={styles.flatListStyle}
+                            numColumns={3}
+                            key={3}
+                            // columnWrapperStyle={{justifyContent: 'space-between'}}
                             renderItem={(obj) => 
-                                <TouchableOpacity style={{flex:0.5, position:'relative'}}
-                                onPress={()=>{navigation.navigate('ThemeDetailScreen', {
+                                <>
+                                <TouchableOpacity style={[{width:117, position:'relative'}, 
+                                    obj.index % 3 == 2 ? {marginRight:0} : {marginRight:20}]}
+                                onPress={()=>{
+                                    if(obj.item.isClear < 2){navigation.navigate('ThemeDetailScreen', {
                                     storeId:obj.item.themeId
-                                })}}>
-                                    <View style={[obj.index !== test.length-1 ? obj.index % 2 == 0 ? {marginRight: 10} : {marginLeft: 10}
-                                        : {marginRight:10}, {height:60, width:186, position:'absolute', bottom:20, zIndex:3, elevation:3, borderBottomLeftRadius:10, borderBottomRightRadius:10, backgroundColor:'white'}]}>
-                                    <CafeThemeTItle>{obj.item.themeName}</CafeThemeTItle>
-                                    </View>
+                                })}else{
+                                    setReviewModal(true)
+                                }
+                                }}>
+                                    <View style={{backgroundColor:'black', height:180, width:117, position:'absolute', zIndex:3, elevation:3, borderRadius:10, opacity:opacity_list[obj.item.isClear]}} />
                                     <ThemeView
                                         source={{uri:obj.item.themeImg}}
                                         resizeMode="cover"
                                         imageStyle={{borderRadius:10}}
-                                        style={
-                                            obj.index !== test.length-1 ? obj.index % 2 == 0 ? {marginRight: 10} : {marginLeft: 10}
-                                        : {marginRight:10}}>
-                                    </ThemeView>
+                                        />
                                 </TouchableOpacity>
-                    }/>
+                                </>
+                    }
+                    
+                    />
                         </View>
                     </>
                     :   
                     <>
-                        <View>
-                            {itemsList.length ? 
-                            <>
-                            <Checkbox value="test" accessibilityLabel="This is a dummy checkbox" />
-                            <Picker
-                            value={selectRegion}
-                            placeholder={'Placeholder'}
-                            onChange={() => setSelectRegion}
-                            >
-                            {itemsList.map((item, index) => (
-                            <Picker.Item key={index} value={item} label={item}/>
-                            ))}
-                            </Picker>
-                            </>
-                        //     <RNPickerSelect
-                        //     style={{inputAndroid: styles.rnpicker}}
-                        //     onValueChange={(value) => setSelectRegion(`${name}/${value}`)}
-                        //     items={itemsList}
-                        //     useNativeAndroidPickerStyle={false}
-                        //     placeholder={{label:'세부지역을 선택해주세요', 'value':null}}
-                        // />
+                        {/* 세부지역이 있을 경우 select box를 보여주고, 없는 경우 보여주지 않음 */}
+                        <View style={{flex:8}}>
+                            {itemsList.length ?
+                            <RNPickerSelect
+                                style={{inputAndroid: styles.rnpicker}}
+                                onValueChange={(value) => {
+                                    setSelectRegion(`${name}/${value}`)
+                                    setSpecificRegion(value)}}
+                                items={itemsList}
+                                value={specificRegion}
+                                useNativeAndroidPickerStyle={false}
+                                placeholder={{label:'세부지역을 선택해주세요', value:null}}
+                            />
                             :
                             null
                             }
-                        
-                        </View>
-                        <>
-                        {selectRegion !== `${name}/null` ? 
-                        <BottomSheetFlatList
-                            numColumns={2}
-                            data={test}
-                            contentContainerStyle={styles.flatListStyle}
-                            renderItem={(obj) => 
-                                <TouchableOpacity style={{flex:0.5, position:'relative'}} onPress={()=>{setCafeSet(obj.item)}}>
-                                    <LinearGradient colors={['rgba(0,0,0,0)', 'rgba(0,0,0,1)']}
-                                    style={[obj.index !== test.length-1 ? obj.index % 2 == 0 ? {marginRight: 10} : {marginLeft: 10}
-                                        : {marginRight:10}, {height:150, width:186, position:'absolute', zIndex:3, elevation:3, borderRadius:10}]} />
-                                    <CafeView
-                                        source={{uri:obj.item.storeImg}}
-                                        resizeMode="cover"
-                                        imageStyle={{borderRadius:10}}
-                                        style={
-                                            obj.index !== test.length-1 ? obj.index % 2 == 0 ? {marginRight: 10} : {marginLeft: 10}
-                                        : {marginRight:10}}><ThemeTitle>{obj.item.storeName}</ThemeTitle>
-                                    </CafeView>
-                                </TouchableOpacity>
-                    }/>
-                        
-                        : null}
-                        </>
+                            {/* 세부지역 선택 시 해당 세부지역에 대한 카페 데이터를 보여줌 */}
+                            {selectRegion !== `${name}/null` ? 
+                            <FlatList
+                                numColumns={2}
+                                data={test}
+                                renderItem={(obj) => 
+                                    <TouchableOpacity style={{flex:0.5, position:'relative'}} onPress={()=>{setCafeSet(obj.item)}}>
+                                        <LinearGradient colors={['rgba(0,0,0,0)', 'rgba(0,0,0,1)']}
+                                        style={[obj.index !== test.length-1 ? obj.index % 2 == 0 ? {marginRight: 10} : {marginLeft: 10}
+                                            : {marginRight:10}, {height:180, width:186, position:'absolute', zIndex:3, elevation:3, borderRadius:10}]} />
+                                        <CafeView
+                                            source={{uri:obj.item.storeImg}}
+                                            resizeMode="cover"
+                                            imageStyle={{borderRadius:10}}
+                                            style={
+                                                obj.index !== test.length-1 ? obj.index % 2 == 0 ? {marginRight: 10} : {marginLeft: 10}
+                                            : {marginRight:10}}>
+                                                <ThemeTitle>진행도</ThemeTitle>
+                                                <ProgressBar progress={55} progressColor={'red'} style={{zIndex:10, marginTop:130}}/>
+                                                {/* <ThemeTitle>{obj.item.storeName}</ThemeTitle> */}
+                                        </CafeView>
+                                    </TouchableOpacity>
+                        }/>
                             
+                            : null}
+                                
+                        </View>
                     </>
                     }
                     {/* </BlurView> */}
-                </BottomSheet>
+                </View>
             </View>
         </ImageBackground>
     )
@@ -273,15 +287,14 @@ const RoomNumber = styled.Text`
 
 const CafeView = styled.ImageBackground`
     flex:0.5;
-    height:150px;
+    height:180px;
     border-radius: 10px;
     padding:${({theme}) => theme.screenMargin.padding};
     /* background-color: white; */
     margin-bottom: 20px;
 `
 const ThemeView = styled.ImageBackground`
-    flex:0.5;
-    height:250px;
+    height:180px;
     margin-bottom: 20px;
 `
 
@@ -292,12 +305,11 @@ const CafeThemeTItle = styled.Text`
 `
 
 const CafeImg = styled.ImageBackground`
-    width:80px;
-    height:80px;
+    width:100px;
+    height:100px;
     margin: auto 0;
 `
 const CafeNavBtn = styled.TouchableOpacity`
-    margin-left: 20px;
     margin-top: auto;
     margin-bottom: auto;
 `
@@ -305,7 +317,9 @@ const CafeNavBtn = styled.TouchableOpacity`
 const CafeName = styled.Text`
     font-family: 'SUIT-Bold';
     font-size: ${({theme}) => theme.fontSizes.title2};
-    margin: auto 20px auto 20px;
+    color:white;
+    margin-bottom: 10px;
+    /* margin: auto 20px auto 20px; */
 `
 
 const ThemeTitle = styled.Text`
@@ -317,6 +331,13 @@ const ThemeTitle = styled.Text`
     position: absolute;
     top:110px;
     left:20px;
+`
+
+const ProgressTitle = styled.Text`
+    font-size: ${({theme}) => theme.fontSizes.body};
+    font-family: 'SUIT-SemiBold';
+    color:white;
+    margin-bottom: 10px;
 `
 
 const styles = StyleSheet.create({
@@ -334,12 +355,12 @@ const styles = StyleSheet.create({
         backgroundColor: 'rgba(255, 255, 255, 0.5)',
     },
     rnpicker:{
-        width:'90%',
+        width:'100%',
         height:50,
         marginLeft:'auto',
         marginRight:'auto',
-        marginTop:20,
-        marginBottom:20,
+        marginTop:30,
+        marginBottom:30,
         backgroundColor:'white',
         borderRadius:10,
         paddingLeft:20,
