9b5ab3f1257d64b3a899425ae577e49a3d5306b8
dm10802 <dm10802@gmail.com>
2022-11-08 01:01:19 +0900

    [S07P31C104-236][FE-윤주]feat:WebGl 로딩 속도 1차 최적화

--- FRONT/escape_web/src/Unity/UnityMainBook.tsx
+++ FRONT/escape_web/src/Unity/UnityMainBook.tsx
@@ -6,9 +6,9 @@ const UnityMainBook = () => {
 
     const { unityProvider } = useUnityContext({
         loaderUrl: "/Builds/Main/Book/Book.loader.js",
-        dataUrl: "/Builds/Main/Book/Book.data",
-        frameworkUrl: "/Builds/Main/Book/Book.framework.js",
-        codeUrl: "/Builds/Main/Book/Book.wasm"
+        dataUrl: "/Builds/Main/Book/Book.data.unityweb",
+        frameworkUrl: "/Builds/Main/Book/Book.framework.js.unityweb",
+        codeUrl: "/Builds/Main/Book/Book.wasm.unityweb"
     })
 
     return (
