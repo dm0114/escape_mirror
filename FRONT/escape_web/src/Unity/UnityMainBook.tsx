81ced345b46dd3245fe6359e3b40ea984c9d06db
dm10802 <dm10802@gmail.com>
2022-11-08 00:14:45 +0900

    [S07P31C104-236][FE-윤주]feat:webgl 서버 테스트

--- FRONT/escape_web/src/Unity/UnityMainBook.tsx
+++ FRONT/escape_web/src/Unity/UnityMainBook.tsx
@@ -1,16 +1,14 @@
-import React, { useEffect } from 'react';
-import {useState} from 'react';
+import React from 'react';
 import { Unity, useUnityContext } from "react-unity-webgl";
-// import {} from '../public/Build/test.framework';
-// import {} from '../../public/Build/test.framework';
+
 
 const UnityMainBook = () => {
 
     const { unityProvider } = useUnityContext({
-        loaderUrl: "/Build/Main/Book/Book.loader.js",
-        dataUrl: "/Build/Main/Book/Book.data",
-        frameworkUrl: "/Build/Main/Book/Book.framework.js",
-        codeUrl: "/Build/Main/Book/Book.wasm"
+        loaderUrl: "/Builds/Main/Book/Book.loader.js",
+        dataUrl: "/Builds/Main/Book/Book.data",
+        frameworkUrl: "/Builds/Main/Book/Book.framework.js",
+        codeUrl: "/Builds/Main/Book/Book.wasm"
     })
 
     return (
