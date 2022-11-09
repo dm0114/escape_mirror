62ef3c867ef04a0b2cdb8900f3b374abdec6f9e0
dm10802 <dm10802@gmail.com>
2022-11-10 02:50:08 +0900

    [S07P31C104-280][BE-수빈] feat : 테마 등록api

--- BACK/sinbangsa/src/main/java/com/sinbangsa/data/repository/ThemeTimeRepository.java
+++ BACK/sinbangsa/src/main/java/com/sinbangsa/data/repository/ThemeTimeRepository.java
@@ -3,6 +3,8 @@ package com.sinbangsa.data.repository;
 import com.sinbangsa.data.dto.ThemeTimeDto;
 import com.sinbangsa.data.entity.ThemeTime;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,6 +15,9 @@ public interface ThemeTimeRepository extends JpaRepository<ThemeTime, Long> {
     ThemeTime findById(long id);
 
     List<ThemeTime> findAllByThemeId(long themeId);
+
+    @Query(value = "select ifnull(max(t.id),0) from theme_time t where t.id > :themeId * 100",nativeQuery = true)
+    Long getNewId(@Param("themeId") long themeId);
 
 
 }
