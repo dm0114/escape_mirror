6e3511cd7d3e9b333a920a17a9862ae72212f74d
dm10802 <dm10802@gmail.com>
2022-11-18 15:02:09 +0900

    [No-issue][FE-윤주]chore: 성공/실패 이미지 추가

--- FRONT/escape_mobile/src/store/SecureStore.js
+++ FRONT/escape_mobile/src/store/SecureStore.js
@@ -5,16 +5,9 @@ const setData = async (key, value) => {
 }
 
 const getData = async (key) => {
-<<<<<<< HEAD
-  console.log(await SecureStore.getItemAsync(key))
-  return await SecureStore.getItemAsync(key);
-=======
   const result = await SecureStore.getItemAsync(key);
   console.log("token", result)
   return result
-
-
->>>>>>> 1c8cdf8582258c0dee0393cc16a6f3383b8612bb
 }
 
 export const SecureState = {setData, getData}
